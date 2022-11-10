59c9f783fa23c4ae5a97e603ad4e61747242d836
pete dekterev <p.dekterev@opti-com.ru>
2022-11-10 17:57:12 +0300

    fix: js

--- app/layout.js
+++ app/layout.js
@@ -2,18 +2,15 @@ import './globals.css';
 
 export default function RootLayout({
   children
-}: {
-    // eslint-disable-next-line no-undef
-  children: React.ReactNode
 }) {
   return (
     <html lang='en'>
-      {/*
+    {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>{children}</body>
+    <head />
+    <body>{children}</body>
     </html>
   );
 }
